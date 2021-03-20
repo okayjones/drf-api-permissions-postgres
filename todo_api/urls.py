7602e699bb8c78dce4f86804b9569fd08e799e59
Logan Jones <loganemilyjones@mac.com>
2021-03-20 13:21:16 -0700

    perms

--- todo_api/urls.py
+++ todo_api/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/tasks/', include('tasks.urls'))
+    path('api/v1/tasks/', include('tasks.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
