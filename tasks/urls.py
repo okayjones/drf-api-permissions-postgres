b2847c759e5a4c72746833a425ca5186e1e9e6d8
Logan Jones <loganemilyjones@mac.com>
2021-03-20 12:56:16 -0700

    serializer, urls, views

---
+++ tasks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import TaskList, TaskDetail
+
+
+urlpatterns = [
+    path('', TaskList.as_view(), name='task_list'),
+    path('<int:pk>', TaskDetail.as_view(), name='task_detail'),
+]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializer import TaskSerializer
+from .models import Task
 
-# Create your views here.
+
+class TaskList(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
