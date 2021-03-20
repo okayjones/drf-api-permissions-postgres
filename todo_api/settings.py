f47a415e7eff35d01287b0002b019b4c2721c5aa
Logan Jones <loganemilyjones@mac.com>
2021-03-20 12:56:01 -0700

    rest framework

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -37,8 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tasks'
+    'rest_framework',
+    'tasks',
 ]
+
+REST_FRAMEWORK = {
+  'DEFAULT_PERMISSION_CLASSES': [
+    'rest_framework.permissions.AllowAny',
+  ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
